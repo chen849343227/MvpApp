32c4d13d9cc75befa6cff00887ec3aef08354107
long <849343227@qq.com>
2017-06-24 22:04:06 +0800

    添加了主界面DrawLayout和Fragment基类。

---
+++ app/src/main/java/com/chen/mvp/module/base/BaseFragment.java
@@ -0,0 +1,189 @@
+package com.chen.mvp.module.base;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.chen.mvp.AndroidApplication;
+import com.chen.mvp.R;
+import com.chen.mvp.injector.component.ApplicationComponent;
+import com.chen.mvp.utils.SwipeRefreshHelper;
+import com.chen.mvp.widget.EmptyLayout;
+import com.orhanobut.logger.Logger;
+import com.trello.rxlifecycle2.LifecycleTransformer;
+import com.trello.rxlifecycle2.components.RxFragment;
+
+import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * 基类碎片
+ *
+ * @param <T>
+ */
+public abstract class BaseFragment<T extends IBasePresenter> extends RxFragment implements IBaseView {
+
+    /**
+     * 注意，资源的ID一定要一样
+     */
+    @Nullable
+    @BindView(R.id.empty_layout)
+    EmptyLayout mEmptyLayout;
+    @Nullable
+    @BindView(R.id.swipe_refresh)
+    SwipeRefreshLayout mSwipeRefresh;
+    @Inject
+    protected T mPresenter;
+
+    protected Context mContext;
+    //缓存Fragment view
+    private View mRootView;
+    private boolean mIsMulti = false;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = getActivity();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        if (mRootView == null) {
+            mRootView = inflater.inflate(attachLayoutRes(), null);
+            ButterKnife.bind(this, mRootView);
+            initInjector();
+            initViews();
+            initSwipeRefresh();
+        }
+        //缓存的rootView需要判断是否已经被加过parent， 如果有parent则从parent删除，防止发生这个rootview已经有parent的错误。
+        ViewGroup parent = (ViewGroup) mRootView.getParent();
+        if (parent != null) {
+            parent.removeView(mRootView);
+        }
+        return mRootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (getUserVisibleHint() && mRootView != null && !mIsMulti) {
+            mIsMulti = true;
+            updateViews(false);
+        }
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        if (isVisibleToUser && isVisible() && mRootView != null && !mIsMulti) {
+            mIsMulti = true;
+            updateViews(false);
+        } else {
+            super.setUserVisibleHint(isVisibleToUser);
+        }
+    }
+
+    @Override
+    public void showLoading() {
+        if (mEmptyLayout != null) {
+            mEmptyLayout.setEmptyStatus(EmptyLayout.STATUS_LOADING);
+            SwipeRefreshHelper.enableRefresh(mSwipeRefresh, false);
+        }
+    }
+
+    @Override
+    public void hideLoading() {
+        if (mEmptyLayout != null) {
+            mEmptyLayout.hide();
+            SwipeRefreshHelper.enableRefresh(mSwipeRefresh, true);
+            SwipeRefreshHelper.controlRefresh(mSwipeRefresh, false);
+        }
+    }
+
+    @Override
+    public void showNetError(final EmptyLayout.OnRetryListener onRetryListener) {
+        if (mEmptyLayout != null) {
+            mEmptyLayout.setEmptyStatus(EmptyLayout.STATUS_NO_NET);
+            mEmptyLayout.setRetryListener(onRetryListener);
+            SwipeRefreshHelper.enableRefresh(mSwipeRefresh, false);
+        }
+    }
+
+    @Override
+    public <T> LifecycleTransformer<T> bindToLife() {
+        return this.<T>bindToLifecycle();
+    }
+
+    @Override
+    public void finishRefresh() {
+        Logger.w("finishRefresh");
+        if (mSwipeRefresh != null) {
+            Logger.e("finishRefresh");
+            mSwipeRefresh.setRefreshing(false);
+        }
+    }
+
+    /**
+     * 获取 ApplicationComponent
+     *
+     * @return ApplicationComponent
+     */
+    protected ApplicationComponent getAppComponent() {
+        return AndroidApplication.getAppComponent();
+    }
+
+    /**
+     * 初始化 Toolbar
+     *
+     * @param toolbar
+     * @param homeAsUpEnabled
+     * @param title
+     */
+    protected void initToolBar(Toolbar toolbar, boolean homeAsUpEnabled, String title) {
+        ((BaseActivity) getActivity()).initToolBar(toolbar, homeAsUpEnabled, title);
+    }
+
+
+    /**
+     * 初始化下拉刷新
+     */
+    public void initSwipeRefresh() {
+        if (mSwipeRefresh != null) {
+            SwipeRefreshHelper.init(mSwipeRefresh, () -> updateViews(true));
+        }
+    }
+
+    /**
+     * 绑定布局文件
+     *
+     * @return 布局文件ID
+     */
+    protected abstract int attachLayoutRes();
+
+    /**
+     * Dagger 注入
+     */
+    protected abstract void initInjector();
+
+    /**
+     * 初始化视图控件
+     */
+    protected abstract void initViews();
+
+    /**
+     * 更新视图控件
+     *
+     * @param isRefresh 新增参数，用来判断是否为下拉刷新调用，下拉刷新的时候不应该再显示加载界面和异常界面
+     */
+    protected abstract void updateViews(boolean isRefresh);
+
+}
--- app/src/main/java/com/chen/mvp/module/home/HomeActivity.java
+++ app/src/main/java/com/chen/mvp/module/home/HomeActivity.java
@@ -1,11 +1,26 @@
 package com.chen.mvp.module.home;
 
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.FrameLayout;
+
 import com.chen.mvp.R;
 import com.chen.mvp.module.base.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 public class HomeActivity extends BaseActivity {
 
+
+    @BindView(R.id.fl_container)
+    FrameLayout flContainer;
+    @BindView(R.id.nav_view)
+    NavigationView navView;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
 
     @Override
     public int attachLayoutRes() {
